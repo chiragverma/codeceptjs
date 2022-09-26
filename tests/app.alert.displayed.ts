5b8a128a0d994d08abb1df53768bf06749f06425
Chirag Verma <chirag.verma@ssense.com>
2022-09-26 20:47:28 +0200

    tests time fixed

--- tests/app.alert.displayed.ts
+++ tests/app.alert.displayed.ts
@@ -1,7 +1,6 @@
 Feature('Alert is displayed');
 
 Scenario('Press the button upon swiping to left and validate an Alert is displayed', ({ I }) => { 
-  //I.swipeRight('~Settings tab');
   I.click('~Settings tab');
   I.seeElement('~Only show losers');
 });
